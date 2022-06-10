533a6a969e7077a7c6e5a8e2424df528ff8bdaa0
Aluno Fatec <aluno@fateclab.net>
2022-06-10 09:37:31 -0300

    Altera tamanho campos tela cadastrar

--- src/Views/Cadastrar.java
+++ src/Views/Cadastrar.java
@@ -72,7 +72,7 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoNomeActionPerformed(evt);
             }
         });
-        jPanel1.add(campoNome, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 240, 620, 10));
+        jPanel1.add(campoNome, new org.netbeans.lib.awtextra.AbsoluteConstraints(92, 234, 630, 21));
 
         campoRua.setBackground(new java.awt.Color(237, 237, 237));
         campoRua.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -82,7 +82,7 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoRuaActionPerformed(evt);
             }
         });
-        jPanel1.add(campoRua, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 310, 230, -1));
+        jPanel1.add(campoRua, new org.netbeans.lib.awtextra.AbsoluteConstraints(92, 306, 265, 20));
 
         campoCidade.setBackground(new java.awt.Color(237, 237, 237));
         campoCidade.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
@@ -92,12 +92,12 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoCidadeActionPerformed(evt);
             }
         });
-        jPanel1.add(campoCidade, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 380, 180, 20));
+        jPanel1.add(campoCidade, new org.netbeans.lib.awtextra.AbsoluteConstraints(92, 380, 185, 20));
 
         campoEmailCad.setBackground(new java.awt.Color(237, 237, 237));
         campoEmailCad.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         campoEmailCad.setBorder(null);
-        jPanel1.add(campoEmailCad, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 450, 290, 20));
+        jPanel1.add(campoEmailCad, new org.netbeans.lib.awtextra.AbsoluteConstraints(92, 451, 295, 20));
 
         jButton1.setFont(new java.awt.Font("Tahoma", 0, 14)); // NOI18N
         jButton1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Botão Cadastrar.png"))); // NOI18N
@@ -115,11 +115,11 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoCEPActionPerformed(evt);
             }
         });
-        jPanel1.add(campoCEP, new org.netbeans.lib.awtextra.AbsoluteConstraints(600, 310, 80, -1));
+        jPanel1.add(campoCEP, new org.netbeans.lib.awtextra.AbsoluteConstraints(604, 306, 124, 20));
 
         campoN.setBackground(new java.awt.Color(237, 237, 237));
         campoN.setBorder(null);
-        jPanel1.add(campoN, new org.netbeans.lib.awtextra.AbsoluteConstraints(390, 310, 30, -1));
+        jPanel1.add(campoN, new org.netbeans.lib.awtextra.AbsoluteConstraints(391, 306, 40, 20));
 
         campoCPF.setBackground(new java.awt.Color(237, 237, 237));
         campoCPF.setBorder(null);
@@ -128,7 +128,7 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoCPFActionPerformed(evt);
             }
         });
-        jPanel1.add(campoCPF, new org.netbeans.lib.awtextra.AbsoluteConstraints(590, 380, 130, 20));
+        jPanel1.add(campoCPF, new org.netbeans.lib.awtextra.AbsoluteConstraints(587, 380, 140, 19));
 
         campoUF.setBackground(new java.awt.Color(237, 237, 237));
         campoUF.setBorder(null);
@@ -137,11 +137,11 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoUFActionPerformed(evt);
             }
         });
-        jPanel1.add(campoUF, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 380, 70, 20));
+        jPanel1.add(campoUF, new org.netbeans.lib.awtextra.AbsoluteConstraints(310, 380, 66, 20));
 
         campoSenhaCad.setBackground(new java.awt.Color(237, 237, 237));
         campoSenhaCad.setBorder(null);
-        jPanel1.add(campoSenhaCad, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 450, 120, 20));
+        jPanel1.add(campoSenhaCad, new org.netbeans.lib.awtextra.AbsoluteConstraints(420, 451, 132, 20));
 
         campoTel.setBackground(new java.awt.Color(237, 237, 237));
         campoTel.setBorder(null);
@@ -150,11 +150,11 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoTelActionPerformed(evt);
             }
         });
-        jPanel1.add(campoTel, new org.netbeans.lib.awtextra.AbsoluteConstraints(410, 380, 140, -1));
+        jPanel1.add(campoTel, new org.netbeans.lib.awtextra.AbsoluteConstraints(408, 380, 145, 19));
 
         campoBairro.setBackground(new java.awt.Color(237, 237, 237));
         campoBairro.setBorder(null);
-        jPanel1.add(campoBairro, new org.netbeans.lib.awtextra.AbsoluteConstraints(470, 310, 90, -1));
+        jPanel1.add(campoBairro, new org.netbeans.lib.awtextra.AbsoluteConstraints(466, 306, 100, 20));
 
         campoConfirmaSenha.setBackground(new java.awt.Color(237, 237, 237));
         campoConfirmaSenha.setBorder(null);
@@ -163,7 +163,7 @@ public class Cadastrar extends javax.swing.JFrame {
                 campoConfirmaSenhaActionPerformed(evt);
             }
         });
-        jPanel1.add(campoConfirmaSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(580, 450, 140, 20));
+        jPanel1.add(campoConfirmaSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(582, 451, 145, 20));
 
         confirmaSenha.setIcon(new javax.swing.ImageIcon(getClass().getResource("/Imagens/Tela Cadastrar2.png"))); // NOI18N
         jPanel1.add(confirmaSenha, new org.netbeans.lib.awtextra.AbsoluteConstraints(10, 20, 1100, 610));
